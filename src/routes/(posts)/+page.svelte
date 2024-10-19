e53775cdf471b79b6b16ad645de0f82dade58a42
Riya Patel <riyap7521@gmail.com>
2024-10-18 23:47:03 -0400

    init

--- src/routes/(posts)/+page.svelte
+++ src/routes/(posts)/+page.svelte
@@ -29,80 +29,31 @@
 </svelte:head>
 <h1>Welcome!</h1>
 <p>
-	Thanks for visiting my blog. I'm a cofounder of <a
-		href="https://www.matrices.app?ref=blog.johnqian.com"
-		target="_blank">Matrices</a
-	>, previously eng at Adept and Weights & Biases. Here I try to distill my thoughts to their
-	simplest forms.
+	My name is Riya Patel. I'm studying software engineering at UWaterloo and have fun working on projects that add value to the world. Currently working on firmware on <a
+		href="https://www.uworbital.com/"
+		target="_blank">Orbital</a
+	> and 
+	<a
+		href="https://www.waterloorocketry.com/"
+		target="_blank">Rocketry</a
+	>.
+
 </p>
+
 <p>
-	If my blog posts resonate with you, feel free to reach out to me at <a
-		href="mailto:johnlongqian+blog@gmail.com">johnlongqian+blog@gmail.com</a
+	Feel free to reach out to me at <a
+		href="mailto:riya.patel1@uwaterloo.ca">riya.patel1@uwaterloo.ca</a
 	>
 </p>
 <div class="flex gap-2 items-center mt-8">
-	<a target="_blank" rel="noreferrer" href="https://twitter.com/johnlqian">
+	<a target="_blank" rel="noreferrer" href="https://x.com/riyapee">
 		<img alt="twitter profile" src="twitter.svg" class="h-4 hover:brightness-110" />
 	</a>
-	<a target="_blank" rel="noreferrer" href="https://github.com/Xyzrr/micro-blog">
+	<a target="_blank" rel="noreferrer" href="https://github.com/riyapatel25">
 		<img alt="github profile" src="github.svg" class="h-5 rounded-full hover:opacity-90" />
 	</a>
 </div>
-<p class="mt-16">
-	Also feel free to put your email here if you want to be notified when I publish something
-	potentially interesting. These emails will be very infrequent, if I send any at all. Nothing is
-	automated; I'm just gauging interest right now.
-</p>
-<!-- Form -->
-<form on:submit|preventDefault={subscribe} class="flex gap-2 items-center">
-	<input
-		type="email"
-		bind:value={email}
-		placeholder="Email address"
-		required
-		class="border rounded px-2 py-1"
-	/>
-	{#if loading}
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="1.5"
-			stroke="currentColor"
-			class="w-6 h-6 animate-spin"
-		>
-			<path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-		</svg>
-	{:else if done}
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke-width="1.5"
-			stroke="currentColor"
-			class="w-6 h-6 text-green-500"
-		>
-			<path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-		</svg>
-	{:else}
-		<button type="submit" class="opacity-80 hover:opacity-100 hover:text-blue-400">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-6 h-6"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-				/>
-			</svg>
-		</button>
-	{/if}
-</form>
+
 {#if done}
 	<p class="mt-2 text-sm text-gray-500">Thank you!</p>
 {/if}
