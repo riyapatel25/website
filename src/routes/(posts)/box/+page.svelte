7b321cda3c788d053b0b046ad8fa6558c665b67f
Riya Patel <riyap7521@gmail.com>
2024-11-18 13:58:59 -0500

    box

---
+++ src/routes/(posts)/box/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Lock Box</title>
+</svelte:head>
+<h1>Lock Box</h1>
+<div class="opacity-60 mb-8">Oct 2024</div>
+<p>
+	Coming Soon
+</p>
--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -5,10 +5,10 @@
 <div class="w-60 shrink-0 h-full">
 	<div class="list-none flex flex-col pt-10 sticky top-0">
 		<SidebarItem to="/">Home</SidebarItem>
+		<SidebarItem to="/box">Lock Box</SidebarItem>
 		<SidebarItem to="/remy">Ratatouille</SidebarItem>
-		<SidebarItem to="/simplicity">Heat Tile Crack Detection Drone</SidebarItem>	
+		<SidebarItem to="/drone">Heat Tile Crack Detection Drone</SidebarItem>
 		<SidebarItem to="/internships">Internships</SidebarItem>
-		<SidebarItem to="/startup-spark">Why Engineering on the Moon is Hard</SidebarItem>
-
+		<SidebarItem to="/moon">Why Engineering on the Moon is Hard</SidebarItem>
 	</div>
 </div>
